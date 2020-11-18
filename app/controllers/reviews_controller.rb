d7c32d06b4c44a61562f2caf25fa3fa6aa194952
ValentinLanglet <valentin.langlet59@gmail.com>
2020-11-18 14:59:26 +0100

    finish
    Signed-off-by: ValentinLanglet <valentin.langlet59@gmail.com>

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    # chopper le restau id et lui coller la review
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+
+    if @review.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do ||
+new_restau = Restaurant.create(name: 'Chocolaterie', address: 'rue chocolat', category: 'choco', phone_number: 'dsd')
+end
+
